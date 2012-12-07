5560bc6e7ce605e6c89b947167ded3de111dd29b
Jay Williams <clubugly@gmail.com>
2012-12-07 16:58:50 -0500

    Index and Map update
    Index - removed old pages as I realized I was working from old
    index.html by accident.
    Map - Updated the view in attempt to get file working correctly

--- asdcouch/asdcouch/app/views/contacts/map.js
+++ asdcouch/asdcouch/app/views/contacts/map.js
@@ -1,17 +1,15 @@
 function(doc) {
-  if (doc._id.substr(0, 6)==="contact" {
-    emit(doc._id,{
-     	"fname":doc.fname,
-     	"email":doc.email,
-     	"url":doc.url,
-    	"sex":doc.sex,
-     	"borndate":doc.borndate,
-     	"groups":doc.groups,
-     	"quantity":doc.quantity,
-     	"comments":doc.comments,
-     	"terms":doc.terms,
-    }    	
-    
-    );
-  }
-};
+	if (doc._id.substr(0,6)=== "contact"){
+    	emit(doc._id, {
+     			"fname":doc.fname,
+     			"email":doc.email,
+     			"url":doc.url,
+    			"sex":doc.sex,
+     			"borndate":doc.borndate,
+     			"groups":doc.groups,
+     			"quantity":doc.quantity,
+     			"comments":doc.comments,
+     			"terms":doc.terms,
+    	});
+  	}
+}
