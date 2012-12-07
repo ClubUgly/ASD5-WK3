77a624c4f8a6994ab45e8514078eba0ab8d55453
Jay Williams <clubugly@gmail.com>
2012-12-07 00:36:24 -0500

    Sigh!!! Cant get it to work right

--- asdcouch/asdcouch/app/views/contacts/map.js
+++ asdcouch/asdcouch/app/views/contacts/map.js
@@ -1,5 +1,17 @@
 function(doc) {
-  if (doc.created_at) {
-    emit(doc.created_at, doc);
+  if (doc._id.substr(o, 6)==="contact" {
+    emit(doc._id,{
+     	"fname":doc.fname,
+     	"email":doc.email,
+     	"url":doc.url,
+    	"sex":doc.sex,
+     	"borndate":doc.borndate,
+     	"groups":doc.Candidate Selection,
+     	"quantity":doc.quantity,
+     	"comments":doc.comments,
+     	"terms":doc.TOS,
+    }    	
+    
+    );
   }
 };
